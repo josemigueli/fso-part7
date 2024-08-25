83bc238654b0f3e6ac04d579b70268d22cc2da46
josemigueli <miguelusx@gmail.com>
2024-08-24 22:53:42 -0600

    Useless files deleted

--- blog-client/src/main.jsx
+++ blog-client/src/main.jsx
@@ -4,7 +4,6 @@ import { UserContextProvider } from './UserContext'
 import { NotificationContextProvider } from './NotificationContext'
 import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App'
-import './index.css'
 
 const queryClient = new QueryClient()
 
